1749034fad9bc82b4f09549a9e21937370e80a34
Sgtfishtank <joh_lau@hotamil.com>
2016-10-03 12:51:38 +0200

    box and gm

---
+++ OverAndUnder/Assets/Scripts/Box.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Box : MonoBehaviour
+{
+    public Camera camera;
+    public List<GameObject> BoxSlots;
+    public int Slot;
+    public GameMaster GM;
+    // Use this for initialization
+    void Start ()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        GM = GameObject.Find("Game Master").GetComponent<GameMaster>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            transform.position = camera.ScreenToWorldPoint(new Vector3(pos.x,pos.y,camera.nearClipPlane));
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            for (int i = 0; i < BoxSlots.Count; i++)
+            {
+                if (Mathf.Abs((transform.position - BoxSlots[i].transform.position).magnitude) <= 0.75f)
+                {
+                    GM.swapBox(Slot, i);
+                    Slot = i;
+                    transform.position = BoxSlots[i].transform.position;
+                }
+            }
+            
+        }
+    }
+    public void newPos(int pos)
+    {
+        Slot = pos;
+        transform.position = BoxSlots[Slot].transform.position;
+    }
+
+}
---
+++ OverAndUnder/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameMaster : MonoBehaviour
+{
+    public List<GameObject> boxes;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    public void swapBox(int CurrentPos, int newPos)
+    {
+        GameObject temp = boxes[newPos];
+        boxes[newPos] = boxes[CurrentPos];
+        boxes[CurrentPos] = temp;
+        boxes[CurrentPos].transform.GetComponent<Box>().newPos(CurrentPos);
+    }
+}
+

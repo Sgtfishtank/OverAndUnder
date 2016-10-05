d61dba7309eb52347f803dd780625e4990b723e8
Sgtfishtank <joh_lau@hotamil.com>
2016-10-05 16:53:43 +0200

    ui done?

--- OverAndUnder/Assets/Scripts/UI.cs
+++ OverAndUnder/Assets/Scripts/UI.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour {
     public Text[] textfilds;
+    public new Camera camera;
     GameMaster GM;
 	// Use this for initialization
 	void Start () {
@@ -14,12 +15,17 @@ public class UI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         textfilds[0].text = GM.boxes[0].transform.GetComponent<Box>().hp.ToString();
-        textfilds[0].rectTransform.position = GM.boxes[0].transform.position;
+        textfilds[0].rectTransform.position = camera.WorldToScreenPoint(GM.boxes[0].transform.localPosition);
         textfilds[1].text = GM.boxes[1].transform.GetComponent<Box>().hp.ToString();
+        textfilds[1].rectTransform.position = camera.WorldToScreenPoint(GM.boxes[1].transform.localPosition);
         textfilds[2].text = GM.boxes[2].transform.GetComponent<Box>().hp.ToString();
-        textfilds[3].text = GM.boxes[3].transform.GetComponent<Box>().hp.ToString();
+        textfilds[2].rectTransform.position = camera.WorldToScreenPoint(GM.boxes[2].transform.localPosition);
+        textfilds[5].text = GM.boxes[3].transform.GetComponent<Box>().hp.ToString();
+        textfilds[5].rectTransform.position = camera.WorldToScreenPoint(GM.boxes[3].transform.localPosition);
         textfilds[4].text = GM.boxes[4].transform.GetComponent<Box>().hp.ToString();
-        textfilds[5].text = GM.boxes[5].transform.GetComponent<Box>().hp.ToString();
+        textfilds[4].rectTransform.position = camera.WorldToScreenPoint(GM.boxes[4].transform.localPosition);
+        textfilds[3].text = GM.boxes[5].transform.GetComponent<Box>().hp.ToString();
+        textfilds[3].rectTransform.position = camera.WorldToScreenPoint(GM.boxes[5].transform.localPosition);
 
         textfilds[6].text = GM.score.ToString();
 	
